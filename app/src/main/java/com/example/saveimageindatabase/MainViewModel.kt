b7a41681cb3ef0256cfb6c38c3b9e238bbdeeaaf
ProkhorovKV <ProkhorovKV@Piteravto.ru>
2021-08-03 12:09:28 +0300

    Добавил переменную для определения папки для хранения картинок в приложении

--- app/src/main/java/com/example/saveimageindatabase/MainViewModel.kt
+++ app/src/main/java/com/example/saveimageindatabase/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.saveimageindatabase
 
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.os.Environment
 import android.util.Log
 import androidx.lifecycle.*
 import androidx.room.Room
@@ -30,16 +31,20 @@ class MainViewModel : ViewModel(), LifecycleObserver {
 
                     val url =
                         URL("http://orgp.spb.ru/wp-content/uploads/2019/11/%D0%BB%D0%B0%D0%B7%D1%83%D1%80%D0%BD%D1%8B%D0%B91.jpg")
-                    val bitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream())
-                    Log.e("TAG", "startDownloadImage: ${bitmap.byteCount}")
+                    val stream = url.openConnection().getInputStream()
+                    val bitmap = BitmapFactory.decodeStream(stream)
+
+                    val sd = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES) // путь к приватной папке, которая будет удалена вместе с приложением, доступны так же другие типы папок
+                    Log.e("TAG", "startDownloadImage: ${sd?.absolutePath}", )
 
                     val imageEntity = ImageEntity(
                         1, "name", url.path, bitmap.convertToByteArray()
                     )
 
                     imageDAO.insertImage(imageEntity)
+
                     val imageFromDB = imageDAO.getAll()
-                    for (image in imageFromDB){
+                    for (image in imageFromDB) {
                         val decodeImage = image.getBitmap()
                         bitmapMutableData.postValue(decodeImage)
                     }
@@ -77,12 +82,9 @@ class MainViewModel : ViewModel(), LifecycleObserver {
         return bytes
     }
 
-    fun String.extensionStr():String{
-        return ""
-    }
 
-    fun ImageEntity.getBitmap(): Bitmap{
-        return BitmapFactory.decodeByteArray(this.bytes,0,this.bytes.size)
+    fun ImageEntity.getBitmap(): Bitmap {
+        return BitmapFactory.decodeByteArray(this.bytes, 0, this.bytes.size)
     }
 
 }
